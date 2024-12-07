95e153adc37e63831b3b5b55462078a7bf7d9659
Masahiro Watanabe <zakkof@MacBook-Pro.local>
2024-12-07 19:17:57 +0900

    update

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -210,13 +210,6 @@ func makePosts(results []Post, csrfToken string, allComments bool) ([]Post, erro
 		}
 
 		p.CSRFToken = csrfToken
-
-		if p.User.DelFlg == 0 {
-			posts = append(posts, p)
-		}
-		if len(posts) >= postsPerPage {
-			break
-		}
 	}
 
 	return posts, nil
